e28912cd2bfe91a0967bd8c9800bc0cb89d1afad
korhanonuk <konuk16@gmail.com>
2016-08-27 15:57:19 +0300

    Count Down versiyon 1.0

---
+++ CountDown/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace CountDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtHour = new System.Windows.Forms.TextBox();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtSec = new System.Windows.Forms.TextBox();
+            this.lblHour = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblSec = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.SuspendLayout();
+            // 
+            // txtHour
+            // 
+            this.txtHour.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtHour.Location = new System.Drawing.Point(58, 86);
+            this.txtHour.Name = "txtHour";
+            this.txtHour.Size = new System.Drawing.Size(58, 35);
+            this.txtHour.TabIndex = 0;
+            // 
+            // txtMin
+            // 
+            this.txtMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMin.Location = new System.Drawing.Point(149, 86);
+            this.txtMin.MaxLength = 2;
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(58, 35);
+            this.txtMin.TabIndex = 1;
+            // 
+            // txtSec
+            // 
+            this.txtSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSec.Location = new System.Drawing.Point(243, 86);
+            this.txtSec.MaxLength = 2;
+            this.txtSec.Name = "txtSec";
+            this.txtSec.Size = new System.Drawing.Size(58, 35);
+            this.txtSec.TabIndex = 2;
+            // 
+            // lblHour
+            // 
+            this.lblHour.AutoSize = true;
+            this.lblHour.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHour.Location = new System.Drawing.Point(81, 151);
+            this.lblHour.Name = "lblHour";
+            this.lblHour.Size = new System.Drawing.Size(26, 29);
+            this.lblHour.TabIndex = 3;
+            this.lblHour.Text = "0";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMin.Location = new System.Drawing.Point(169, 151);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(26, 29);
+            this.lblMin.TabIndex = 4;
+            this.lblMin.Text = "0";
+            // 
+            // lblSec
+            // 
+            this.lblSec.AutoSize = true;
+            this.lblSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSec.Location = new System.Drawing.Point(260, 151);
+            this.lblSec.Name = "lblSec";
+            this.lblSec.Size = new System.Drawing.Size(26, 29);
+            this.lblSec.TabIndex = 5;
+            this.lblSec.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(125, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = ":";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(219, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 29);
+            this.label5.TabIndex = 7;
+            this.label5.Text = ":";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStart.Location = new System.Drawing.Point(134, 218);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(81, 32);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 770;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(12, 30);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(348, 13);
+            this.labelControl1.TabIndex = 9;
+            this.labelControl1.Text = "Lütfen geri saymak istediğiniz süreyi saat, dakika ve saniye olarak giriniz.";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl2.Location = new System.Drawing.Point(73, 67);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(26, 16);
+            this.labelControl2.TabIndex = 10;
+            this.labelControl2.Text = "Saat";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl3.Location = new System.Drawing.Point(158, 67);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(37, 16);
+            this.labelControl3.TabIndex = 11;
+            this.labelControl3.Text = "Dakika";
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl4.Location = new System.Drawing.Point(248, 67);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(38, 16);
+            this.labelControl4.TabIndex = 12;
+            this.labelControl4.Text = "Saniye";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 328);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblSec);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.lblHour);
+            this.Controls.Add(this.txtSec);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.txtHour);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHour;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtSec;
+        private System.Windows.Forms.Label lblHour;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblSec;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+    }
+}
+
